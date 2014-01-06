12cd3bb910044e58062b63cd36be07838a982fa7
Peter de Croos <cultofmetatron@aumlogic.com>
2014-01-05 16:08:36 -0800

    initial commit

---
+++ public/javascripts/init.js
@@ -0,0 +1,21 @@
+
+(function(window, $, _, Backbone, undefined) {
+  
+  window.templates = {};
+
+  var $sources = $('script[type="text/template"]');
+  $sources.each(function(index, el) {
+    var $el = $(el);
+    templates[$el.data('name')] = _.template($el.html());
+  });
+
+
+
+
+
+
+
+
+
+}).call(this, window, jQuery, _, Backbone);
+
